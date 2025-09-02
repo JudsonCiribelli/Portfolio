29b786ef70c76b9bd5e636e869c705cfbc57208d
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-09-02 15:46:30 -0300

    feat: add link component

---
+++ src/app/_components/linksComponent/linkComponent.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+interface LinkComponentProps {
+  href: string;
+  name: string;
+  target?: string;
+}
+
+const LinkComponent = ({ href, name, target }: LinkComponentProps) => {
+  return (
+    <Link href={href} target={target}>
+      <li className="text-lg  border border-white rounded-lg p-2 text-white">
+        {name}
+      </li>
+    </Link>
+  );
+};
+
+export default LinkComponent;
