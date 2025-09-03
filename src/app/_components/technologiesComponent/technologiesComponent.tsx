977bc45361176a5c13d81a795fe988753483ff7a
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-09-02 21:58:18 -0300

    feat: add technologies component

---
+++ src/app/_components/technologiesComponent/technologiesComponent.tsx
@@ -0,0 +1,168 @@
+import { ShimmerButton } from "@/app/magicui/shimmer-button";
+import Image from "next/image";
+
+const TechnologiesComponent = () => {
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      <ShimmerButton className="shadow-2xl">
+        <div className="flex gap-2">
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+            Typerscript
+          </span>
+          <Image
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
+            width={20}
+            height={20}
+            alt="ts icon"
+          />
+        </div>
+      </ShimmerButton>
+      <ShimmerButton className="shadow-2xl">
+        <div className="flex gap-2">
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+            Javascript
+          </span>
+          <Image
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+            width={20}
+            height={20}
+            alt="js icon"
+          />
+        </div>
+      </ShimmerButton>
+      <ShimmerButton className="shadow-2xl">
+        <div className="flex gap-2">
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+            React
+          </span>
+          <Image
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+            width={20}
+            height={20}
+            alt="react icon"
+          />
+        </div>
+      </ShimmerButton>
+      <ShimmerButton className="shadow-2xl">
+        <div className="flex gap-2">
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+            Next
+          </span>
+          <Image
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg"
+            width={20}
+            height={20}
+            alt="next icon"
+            className="bg-white rounded-full p-0.4"
+          />
+        </div>
+      </ShimmerButton>
+      <ShimmerButton className="shadow-2xl">
+        <div className="flex gap-2">
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+            Git
+          </span>
+          <Image
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
+            width={20}
+            height={20}
+            alt="git icon"
+          />
+        </div>
+      </ShimmerButton>
+      <ShimmerButton className="shadow-2xl">
+        <div className="flex gap-2">
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+            HTML
+          </span>
+          <Image
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+            width={20}
+            height={20}
+            alt="HTML icon"
+          />
+        </div>
+      </ShimmerButton>
+      <ShimmerButton className="shadow-2xl">
+        <div className="flex gap-2">
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+            CSS
+          </span>
+          <Image
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
+            width={20}
+            height={20}
+            alt="CSS icon"
+          />
+        </div>
+      </ShimmerButton>
+      <ShimmerButton className="shadow-2xl">
+        <div className="flex gap-2">
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+            Tailwind
+          </span>
+          <Image
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
+            width={20}
+            height={20}
+            alt="Tailwind icon"
+          />
+        </div>
+      </ShimmerButton>
+      <ShimmerButton className="shadow-2xl">
+        <div className="flex gap-2">
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+            Node
+          </span>
+          <Image
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg"
+            width={20}
+            height={20}
+            alt="Node icon"
+          />
+        </div>
+      </ShimmerButton>
+      <ShimmerButton className="shadow-2xl">
+        <div className="flex gap-2">
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+            Postgres
+          </span>
+          <Image
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg"
+            width={20}
+            height={20}
+            alt="Postgres icon"
+          />
+        </div>
+      </ShimmerButton>
+      <ShimmerButton className="shadow-2xl">
+        <div className="flex gap-2">
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+            MySQL
+          </span>
+          <Image
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg"
+            width={20}
+            height={20}
+            alt="MySQL icon"
+          />
+        </div>
+      </ShimmerButton>
+      <ShimmerButton className="shadow-2xl">
+        <div className="flex gap-2">
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+            Jest
+          </span>
+          <Image
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg"
+            width={20}
+            height={20}
+            alt="Jest icon"
+          />
+        </div>
+      </ShimmerButton>
+    </div>
+  );
+};
+
+export default TechnologiesComponent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import HeaderComponent from "@/app/_components/headerComponent/headerComponent";
 import ImageComponent from "./_components/imageComponent/imageComponent";
 import { TextAnimate } from "./magicui/text-animate";
 import InitalApresentation from "./_components/initialApresentationComponent/initialApresentation";
+// import { BorderBeam } from "./magicui/border-beam";
+import TechnologiesComponent from "./_components/technologiesComponent/technologiesComponent";
 
 const Home = () => {
   const { resolvedTheme } = useTheme();
@@ -23,28 +25,45 @@ const Home = () => {
         <Particles
           className="absolute inset-0 z-0"
           quantity={50}
+          staticity={1000}
+          size={1}
           ease={50}
           color={color}
         />
-        <section className="flex h-[900px] w-full overflow-hidden ">
-          <div className="flex items-center justify-center  w-full">
+        <section className="flex h-[850px] w-full overflow-hidden  ">
+          <div className="flex flex-col items-center justify-center w-full">
             {/* Nome */}
+            <h1 className="text-white text-7xl">Judson Ciribelli</h1>
             <TextAnimate
               animation="slideUp"
               as="h1"
               by="word"
               duration={1}
-              className="text-white text-5xl"
+              className="text-white text-6xl"
             >
-              Olá, me chamo Judson Ciribelli sou desenvolvedor Front-end.
+              {`i'm a software engineer`}
             </TextAnimate>
+            {/* <p>Quem é voce? </p>
+            <p>O que voce faz</p>
+            <p>O que voce ja fez</p> */}
           </div>
         </section>
 
         {/* ABOUT */}
-        <section className="w-full flex items-center justify-evenly h-[900px]">
-          <InitalApresentation />
-          <ImageComponent />
+        <section className=" y-8 w-full flex items-center justify-center h-[800px]">
+          <div className="flex relative w-[1800px] items-center justify-evenly rounded-lg p-10 [box-shadow:-4px_-43px_49px_-5px_rgba(148,96,10,0.24)]">
+            <InitalApresentation />
+            <ImageComponent />
+            {/* <BorderBeam duration={12} size={80} className="via-white" /> */}
+          </div>
+        </section>
+
+        <section className="w-full h-[900px] mt-6">
+          <h1 className="text-white text-7xl text-center">Tecnologias</h1>
+          <div className="flex items-center justify-between w-full mt-10">
+            <TechnologiesComponent />
+            <div></div>
+          </div>
         </section>
       </main>
     </>
