cd31ec5bf89603cabc865569b827a64625f2a14b
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-10-06 22:31:35 -0300

    feat: add inventory component with projeect info

---
+++ src/app/_components/Inventory-Description/inventoryDescription.tsx
@@ -0,0 +1,23 @@
+const InventoryDescription = () => {
+  return (
+    <div className="p-4 space-y-6 flex flex-col">
+      <h1 className="text-xl text-white">Inventory Manager</h1>
+      <p className="text-white">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor maiores
+        quidem totam, quaerat necessitatibus sit veniam? Corrupti aut libero
+        minima suscipit veniam? Necessitatibus incidunt et, unde dolorem facere
+        provident ipsum! Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Doloribus ducimus vel id quis maxime molestias dolorem iste
+        sapiente! Mollitia vitae doloremque, adipisci aliquid nisi incidunt
+        nulla rem laboriosam illo ipsam.
+      </p>
+      <div>
+        <h2 className="text-white">Tecnologias utilizadas</h2>
+      </div>
+      <p className="text-white">Link para o github</p>
+      <p className="text-white">Veja o projeto em tempo real</p>
+    </div>
+  );
+};
+
+export default InventoryDescription;
