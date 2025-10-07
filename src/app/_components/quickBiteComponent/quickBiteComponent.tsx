b7781555f84ac59648fb32dd5eed50a2660cbdee
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-10-06 22:31:48 -0300

    feat: add Quick Bite component with projeect info

---
+++ src/app/_components/quickBiteComponent/quickBiteComponent.tsx
@@ -0,0 +1,80 @@
+import {
+  Carousel,
+  CarouselNext,
+  CarouselPrevious,
+  CarouselContent,
+  CarouselItem,
+} from "../ui/carousel";
+import Image from "next/image";
+
+const QuickBiteComponent = () => {
+  return (
+    <Carousel className="w-[45%] h-[400px]">
+      <CarouselContent>
+        <CarouselItem>
+          <Image
+            src="/Quick-Bite/Dashboard.png"
+            alt="project image"
+            width={900}
+            height={900}
+            className="object-cover rounded-lg"
+          />
+        </CarouselItem>
+
+        <CarouselItem className="text-white">
+          <Image
+            src="/Quick-Bite/Cart.png"
+            alt="project image"
+            width={900}
+            height={900}
+            className="object-cover rounded-lg"
+          />
+        </CarouselItem>
+
+        <CarouselItem className="text-white">
+          <Image
+            src="/Quick-Bite/Menu.png"
+            alt="project image"
+            width={900}
+            height={900}
+            className=" rounded-lg"
+          />
+        </CarouselItem>
+
+        <CarouselItem className="text-white">
+          <Image
+            src="/Quick-Bite/Menu2.png"
+            alt="project image"
+            width={900}
+            height={900}
+            className="object-cover rounded-lg"
+          />
+        </CarouselItem>
+
+        <CarouselItem className="text-white">
+          <Image
+            src="/Quick-Bite/Login.png"
+            alt="project image"
+            width={900}
+            height={900}
+            className="object-cover rounded-lg"
+          />
+        </CarouselItem>
+
+        <CarouselItem>
+          <Image
+            src="/Quick-Bite/Order.png"
+            alt="project image"
+            width={900}
+            height={900}
+            className="object-cover rounded-lg"
+          />
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+};
+
+export default QuickBiteComponent;
