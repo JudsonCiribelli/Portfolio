1f52ce3a4f512e970a309211f3dfb60ebaeb8452
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-12-17 12:45:36 -0300

    style: add responsitive to header component

--- src/app/_components/headerComponent/headerComponent.tsx
+++ src/app/_components/headerComponent/headerComponent.tsx
@@ -3,14 +3,14 @@ import LinkComponent from "../linksComponent/linkComponent";
 
 const HeaderComponent = () => {
   return (
-    <header className="w-full h-[90px] flex items-center justify-around  fixed bg-black border-b border-gray-700">
+    <header className="w-full h-20 flex items-center justify-around  bg-black border-b border-gray-700">
       <Link
         href="/"
         className="p-2 text-lg border border-white text-white rounded-lg"
       >
         <p>JC</p>
       </Link>
-      <div className="flex items-center gap-8 list-none">
+      <div className="flex items-center gap-4 list-none xl:gap-8">
         <LinkComponent href="/" name="Sobre" />
         <LinkComponent href="/" name="Projetos" />
         <LinkComponent href="/" name="Contato" />
