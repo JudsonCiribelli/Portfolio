a1011abb9946124e05f6dfc2567e4303eebcf339
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-12-17 21:33:05 -0300

    refactor: update user apresentation

--- src/app/_components/initialApresentationComponent/initialApresentation.tsx
+++ src/app/_components/initialApresentationComponent/initialApresentation.tsx
@@ -3,16 +3,20 @@ import Link from "next/link";
 
 const InitalApresentation = () => {
   return (
-    <div className="flex flex-col max-w-[400px] gap-4">
+    <div className="flex flex-col max-w-[400px] gap-4 xl:max-w-[800px]">
       <h1 className="text-white text-4xl font-normal">Sobre mim</h1>
-      <p className="text-white text-lg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quia
-        possimus earum at quasi hic repellat quam rerum <br /> excepturi
-        veritatis necessitatibus temporibus a dicta totam animi nam quod, ea
-        tenetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id
-        quidem cumque hic harum iste fuga dicta aliquam voluptatem libero.
-        Ratione sit est consequuntur rerum repellendus earum esse itaque. Illo,
-        magnam.
+      <p className="text-white text-sm xl:text-2xl">
+        Mais do que apenas escrever código, sou um profissional movido pela
+        resolução de problemas. Graduado em Interdisciplinar em Ciências e
+        Tecnologia , trago para a tecnologia a mesma disciplina inegociável que
+        aplico na minha vida pessoal: organização, pontualidade e atenção aos
+        detalhes. Como Desenvolvedor Fullstack, atuo construindo a experiência
+        completa. Tenho facilidade em transitar entre o desenvolvimento de
+        interfaces visuais e a arquitetura de regras de negócio complexas no
+        servidor. Meu foco é garantir que o sistema não seja apenas bonito por
+        fora, mas estruturado, seguro e eficiente por dentro. Minhas
+        experiências são fundamentadas na prática intensa. Desenvolvi aplicações
+        completas do zero.
       </p>
       <div className="flex gap-2">
         <Link
